9b8b01f6d34b948f6fd6e65b6f10984cedb6df6f
Haipo <hapioootw@gmail.com>
2025-07-17 17:47:00 +0800

    新增header

--- aromalogic/src/layouts/default.vue
+++ aromalogic/src/layouts/default.vue
@@ -1,18 +1,26 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-img
-        alt="AromaMind Logo"
-        class="shrink mr-2"
-        contain
-        src="@/assets/logo.png"
-        transition="scale-transition"
-        width="40"
-      />
-      <v-toolbar-title>芳療智匯 AromaMind</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <!-- 可以放一些導航按鈕或使用者資訊 -->
+      <v-container class="fill-height d-flex align-center">
+        <v-toolbar-title class="text-left flex-grow-1">
+          芳療智匯 AromaMind
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-img
+          alt="AromaMind Logo"
+          class="shrink mx-auto"
+          contain
+          src="@/assets/logo.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-spacer></v-spacer>
+
+        <span class="text-right flex-grow-1 mr-4">{{ userEmail }}</span>
+      </v-container>
     </v-app-bar>
 
     <v-main>
@@ -24,5 +32,26 @@
 </template>
 
 <script lang="ts" setup>
-  //
+import { ref, onMounted, onUnmounted } from 'vue';
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+
+const userEmail = ref<string | null>(null);
+let unsubscribe: () => void;
+
+onMounted(() => {
+  const auth = getAuth();
+  unsubscribe = onAuthStateChanged(auth, (user: User | null) => {
+    if (user) {
+      userEmail.value = user.email;
+    } else {
+      userEmail.value = null;
+    }
+  });
+});
+
+onUnmounted(() => {
+  if (unsubscribe) {
+    unsubscribe();
+  }
+});
 </script>
